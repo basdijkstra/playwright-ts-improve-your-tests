5498d554f3088b54e4d7eac048d0cf0ec666b999
Bas Dijkstra <bas@ontestautomation.com>
2025-09-17 20:17:44 +0200

    Initial commit

---
+++ tests/loanapplication.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('Loan application for 10000 with down payment of 500 is denied', async ({ page }) => {
+
+  await page.goto('https://parabank.parasoft.com');
+
+  await page.getByRole('link', { name: 'Admin Page'}).click();
+  await page.locator("//button[@value='INIT']").click();
+  await expect(page.locator("//b[text()='Database Initialized']")).toBeVisible();
+
+  await page.locator("//input[@name='username']").fill('john');
+  await page.locator("//input[@name='password']").fill('demo');
+  await page.locator("//input[@value='Log In']").click();
+
+  await page.getByRole('link', { name: 'Request Loan'}).click();
+
+  await page.locator("//input[@id='amount']").fill('10000');
+  await page.locator("//input[@id='downPayment']").fill('1000');
+  await page.locator("//select[@id='fromAccountId']").selectOption('12345');
+  await page.locator("//input[@value='Apply Now']").click();
+
+  await expect(page.locator("//td[@id='loanStatus']")).toHaveText('Denied');
+});
