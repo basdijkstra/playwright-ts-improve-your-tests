1070b1ffee7946c11c401477f31b428fecf633be
Bas Dijkstra <bas@ontestautomation.com>
2025-12-02 12:54:57 +0100

    Add exercise 3: parameterized tests

--- tests/exercises/exercises03.spec.ts
+++ tests/exercises/exercises03.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from './pages/loginPage';
-import { AccountsOverviewPage } from './pages/accountsOverviewPage';
-import { RequestLoanPage } from './pages/requestLoanPage';
+import { LoginPage } from '../answers/pages/loginPage';
+import { AccountsOverviewPage } from '../answers/pages/accountsOverviewPage';
+import { RequestLoanPage } from '../answers/pages/requestLoanPage';
 
+/**
+ * TODO: Refactor these two tests into a single, parameterized test
+ * Which values differ from one test to the other? Pass those in as parameters
+ * These can be input values, but also expected output values
+ */
 test('Loan application for 10000 with down payment of 1000 is denied', async ({ page }) => {
 
   await page.goto('https://parabank.parasoft.com');
@@ -11,34 +16,14 @@ test('Loan application for 10000 with down payment of 1000 is denied', async ({ 
   await page.getByRole('button', { name: 'INIT' }).click();
   await expect(page.getByText('Database Initialized')).toBeVisible();
 
-  /**
-   * TODO: Replace this code with code that uses the LoginPage object.
-   * This class has already been defined. First, call the Open() method,
-   * then the LoginAs() method, passing in the credentials required to successfully log in.
-   */
   var loginPage = new LoginPage(page);
   await loginPage.open();
   await loginPage.loginAs('john', 'demo');
 
-  /**
-   * TODO: Replace this code with code that uses the AccountsOverviewPage object.
-   * This class has already been defined. Call the selectMenuItem() method to
-   * navigate to the 'Request Loan' page.
-   */
   await new AccountsOverviewPage(page).selectMenuItem('Request Loan');
 
-  /**
-   * TODO: Replace this code with code that uses the RequestLoanPage object.
-   * You will need to define this class yourself.
-   * 
-   * Add a method submitLoanRequestFor() that takes the values needed to
-   * submit a loan request and performs the required interactions.
-   */
   await new RequestLoanPage(page).submitLoanRequestFor('10000', '1000', '12345');
 
-  /**
-   * We'll look at alternatives for this statement below together after the exercise
-   */
   await expect(page.locator('td[id=loanStatus]')).toHaveText('Denied');
 });
 
