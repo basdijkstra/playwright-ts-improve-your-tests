5b23259681bb830830de412ef4fd0d28f33fb120
Bas Dijkstra <bas@ontestautomation.com>
2025-12-04 16:49:34 +0100

    Add example for composition

--- tests/answers/pages/accountsOverviewPage.ts
+++ tests/answers/pages/accountsOverviewPage.ts
@@ -1,14 +1,17 @@
 import { Page } from "@playwright/test";
+import { MenuComponent } from "../components/menuComponent";
 
 export class AccountsOverviewPage {
 
     private readonly page: Page;
+    private readonly menu: MenuComponent;
 
     constructor(page: Page) {
         this.page = page;
+        this.menu = new MenuComponent(this.page);
     }
 
     async selectMenuItem(menuitem: string) {
-        await this.page.getByRole('link', { name: menuitem }).click();
+        await this.menu.selectMenuItem(menuitem);
     }
 }
